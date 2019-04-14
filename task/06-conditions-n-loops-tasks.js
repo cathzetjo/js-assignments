909e7971cd46baa51441c51fd8c801fb63a980f5
Katerina Zhadko <katerinkaj@icloud.com>
2019-04-14 19:37:51 +0300

    feat: add solution for task 10 'reverseInteger'

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -252,7 +252,7 @@ function reverseString(str) {
  *   34143 => 34143
  */
 function reverseInteger(num) {
-  throw new Error('Not implemented');
+  return num.toString().split('').reverse().join('');
 }
 
 
