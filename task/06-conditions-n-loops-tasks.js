1419958c3e0d6ab4ebc16f3a39d2d3c17a4a094a
Katerina Zhadko <katerinkaj@icloud.com>
2019-04-14 19:01:21 +0300

    feat: add solution for task 1 'getFizzBuzz'

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -30,7 +30,10 @@
  *
  */
 function getFizzBuzz(num) {
-    throw new Error('Not implemented');
+    if (num%3==0 && num%5==0) {return 'FizzBuzz';}
+    else if (num%3==0) {return 'Fizz';}
+    else if (num%5==0) {return 'Buzz';}
+    else {return num};
 }
 
 
