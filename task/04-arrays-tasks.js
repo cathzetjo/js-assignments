d2c83c23145ab33166e1cbe084f04eaa753a3864
Katerina Zhadko <katerinkaj@icloud.com>
2019-04-14 04:58:21 +0300

    feat: add solution for tasl 6 'removeFalsyValues'

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -108,7 +108,7 @@ function getArrayOfStrings(arr) {
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
 function removeFalsyValues(arr) {
-  throw new Error('Not implemented');
+  return arr.filter(Boolean);
 }
 
 /**
