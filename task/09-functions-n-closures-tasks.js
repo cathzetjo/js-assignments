0d94a89269058538496ac72b79e0abccde8b4ba0
Katerina Zhadko <katerinkaj@icloud.com>
2019-04-14 20:30:49 +0300

    feat: add solution for task 2 'getPowerFunction'

--- task/09-functions-n-closures-tasks.js
+++ task/09-functions-n-closures-tasks.js
@@ -49,7 +49,9 @@ function getComposition(f,g) {
  *
  */
 function getPowerFunction(exponent) {
-    throw new Error('Not implemented');
+    return (number) => {
+        return Math.pow(number, exponent);
+    };
 }
 
 
