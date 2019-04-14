8aacba558918e4b0cc6665ad8e7bc32af51da0f2
Katerina Zhadko <katerinkaj@icloud.com>
2019-04-14 19:16:06 +0300

    feat: add solution for task 3 'getSumBetweenNumbers'

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -76,7 +76,12 @@ function getFactorial(n) {
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
 function getSumBetweenNumbers(n1, n2) {
-  throw new Error('Not implemented');
+  let sum=0;
+  for (let i = n1; i<n2+1; i++)
+  {
+    sum=sum+i;
+  }
+  return sum;
 }
 
 
